7d9497f283bb9e9c6bda974015134d56c776ad7c
Sierra Sutherland-Brown <sierrabardot@live.com.au>
2023-12-09 15:16:58 +1100

    Update recipes/new template

--- public/JS/main.js
+++ public/JS/main.js
@@ -1,28 +1,22 @@
+/*---------- Cache HTML elements ----------*/
+const ingtContainerEl = document.querySelector('.ingredients-container');
+const ingtInfoEl = document.querySelector('.ingt-info');
 const addIngredientBtn = document.querySelector('.add-ingt-btn');
-const containerEl = document.querySelector('.ingredients-container');
-const selectIngtEl = document.querySelector('.select-ingt');
+const methodContainerEl = document.querySelector('.method-container');
+const methodStepEl = document.querySelector('.method-step');
+const addStepBtn = document.querySelector('.add-step-btn');
 
-function addIngredient() {
-    console.log(`Adding ingredient...`);
-    const newIngredientOptions = document
-        .createElement('option')
-        .classList.add('ingt-options')
-        .setAttribute('name', 'prooduct[]');
-    const selectNewIngredient = document
-        .createElement('select')
-        .classList.add('select-ingt')
-        .setAttribute('name', 'ingredient')
-        .appendChild(newIngredientOptions);
-    // newIngredient.innerHTML = selectIngtEl.innerHTML;
-    containerEl.appendChild(selectNewIngredient);
-    console.log(`Ingredient added: ${newIngredient}`);
+/*---------- Event listeners ----------*/
+addIngredientBtn.addEventListener('click', addIngredient);
+addStepBtn.addEventListener('click', addStep);
+
+/*---------- Functions ----------*/
+function addStep() {
+    const clone = methodStepEl.cloneNode(true);
+    methodContainerEl.appendChild(clone);
 }
 
-// create new .select-ingt element and append to .ingredients-container
-// create new .ingt-options element and append to .select-ingt
-// create new .add-qty element and append to .ingredients-container
-
-// create new .add-ingt-button and append to .ingredients-container
-// remove add-ingt-btn
-
-addIngredientBtn.addEventListener('click', addIngredient);
+function addIngredient() {
+    const clone = ingtInfoEl.cloneNode(true);
+    ingtContainerEl.appendChild(clone);
+}
